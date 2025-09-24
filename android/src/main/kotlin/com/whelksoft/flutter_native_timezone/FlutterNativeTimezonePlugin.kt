52378dc9dff237c6975c8f67173a8e46a3bc6960
Geekers Technologies <geekerstechnologies@Geekerss-MacBook-Air.local>
2025-09-24 16:36:06 +0530

    Add modern Flutter plugin dependencies and linting

--- android/src/main/kotlin/com/whelksoft/flutter_native_timezone/FlutterNativeTimezonePlugin.kt
+++ android/src/main/kotlin/com/whelksoft/flutter_native_timezone/FlutterNativeTimezonePlugin.kt
@@ -1,4 +1,4 @@
-package com.whelksoft.flutter_native_timezone
+/* package com.whelksoft.flutter_native_timezone
 
 import android.os.Build
 import androidx.annotation.NonNull
@@ -57,6 +57,83 @@ class FlutterNativeTimezonePlugin : FlutterPlugin, MethodCallHandler {
             ZoneId.getAvailableZoneIds().toCollection(ArrayList())
         } else {
             TimeZone.getAvailableIDs().toCollection(ArrayList())
+        }
+    }
+
+    private fun setupMethodChannel(messenger: BinaryMessenger) {
+        channel = MethodChannel(messenger, "flutter_native_timezone")
+        channel.setMethodCallHandler(this)
+    }
+}
+*/
+
+package com.whelksoft.flutter_native_timezone
+
+import android.os.Build
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry.Registrar
+import java.time.ZoneId
+import java.util.*
+
+class FlutterNativeTimezonePlugin : FlutterPlugin, MethodCallHandler {
+
+    private lateinit var channel: MethodChannel
+
+    companion object {
+        @JvmStatic
+        fun registerWith(registrar: Registrar) {
+            val plugin = FlutterNativeTimezonePlugin()
+            plugin.setupMethodChannel(registrar.messenger())
+        }
+    }
+
+    override fun onAttachedToEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        setupMethodChannel(binding.binaryMessenger)
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        when (call.method) {
+            "getLocalTimezone" -> {
+                try {
+                    result.success(getLocalTimezone())
+                } catch (e: Exception) {
+                    result.error("TIMEZONE_ERROR", "Failed to get local timezone", e.message)
+                }
+            }
+            "getAvailableTimezones" -> {
+                try {
+                    result.success(getAvailableTimezones())
+                } catch (e: Exception) {
+                    result.error("TIMEZONE_ERROR", "Failed to get available timezones", e.message)
+                }
+            }
+            else -> result.notImplemented()
+        }
+    }
+
+    private fun getLocalTimezone(): String {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            ZoneId.systemDefault().id
+        } else {
+            TimeZone.getDefault().id
+        }
+    }
+
+    private fun getAvailableTimezones(): List<String> {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            ZoneId.getAvailableZoneIds().toList().sorted()
+        } else {
+            TimeZone.getAvailableIDs().toList().sorted()
         }
     }
 
